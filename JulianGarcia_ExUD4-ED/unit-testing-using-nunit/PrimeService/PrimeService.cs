c933422ecec70896c9efe6c8b5130c35b8af7b62
jgarvel076 <115531628+jgarvel076@users.noreply.github.com>
2023-02-15 11:41:22 +0100

    examen

---
+++ JulianGarcia_ExUD4-ED/unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
